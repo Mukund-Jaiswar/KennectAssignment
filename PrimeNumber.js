4bfab1948d74ab3a2ff119248491fe6e2789e2ad
Mukund-Jaiswar <mukundjaiswar2001@gmail.com>
2022-11-26 21:04:22 +0530

    Intial Commit

---
+++ PrimeNumber.js
@@ -0,0 +1,43 @@
+//Function for number to find out if it is a prime number or not
+function isPrimeNum(num){
+    let flag = true
+
+    if(num<=0){
+        return 'It is a -ve Number'
+    }else if(num===1){
+        return 'one is not a prime number'
+    }else{
+        for(let i=2;i<num;i++){
+            if(num%i===0){
+                flag = false
+                break;
+            }
+        }
+    }
+    return flag
+}
+
+//Finding next Prime Number
+function nextPrimeNum(num){
+    num = num+1
+    while(!isPrimeNum(num)){
+        num++;
+    }
+    return num
+}
+
+//If Prime number then Difference with next prime number
+//Main Function
+function isPrimeAndNextDiff(num){
+    
+    if(isPrimeNum(num)){
+        let diff = nextPrimeNum(num)-num
+        return console.log(`${num} is a Prime Number and the diff with next prime number is: ${diff}`)
+    }else{
+        return console.log(`${num} is not a Prime Number`)
+    }
+}
+
+isPrimeAndNextDiff(23)
+//Output
+//23 is a Prime Number and the diff with next prime number is: 6
